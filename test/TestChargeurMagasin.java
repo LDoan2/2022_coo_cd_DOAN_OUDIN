4813af5127c459e4eb3c362013178d529fa8f14d
Clement Oudin <clementoudin3@gmail.com>
2022-05-10 11:10:18 +0200

    test

--- test/TestChargeurMagasin.java
+++ test/TestChargeurMagasin.java
@@ -1,9 +1,23 @@
+import XML.ChargeurMagasin;
+import donnees.Magasin;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestChargeurMagasin {
     @Test
-    public void test1(){
-
+    public void test1() throws FileNotFoundException {
+        boolean error = false;
+        try {
+            String repertoire = "../magasinCD_donnees/musicbrainzSimple/";
+            ChargeurMagasin charge = new ChargeurMagasin(repertoire);
+            Magasin resultat = charge.chargerMagasin();
+        }catch (FileNotFoundException e){
+            error = true;
+        }
+        assertEquals(false, error, "Repertoire incorrect");
     }
 
 }
