3652968d343f42db98c8ae169bd4afd9dd40330c
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-05-17 11:02:17 +0200

    Comparateur album

--- src/donnees/ComparateurAlbum.java
+++ src/donnees/ComparateurAlbum.java
@@ -6,7 +6,7 @@ public class ComparateurAlbum implements ComparateurCd{
     public boolean etreAvant(CD cd1, CD cd2) {
         boolean res = false;
 
-        if (cd1.getNomArtiste().compareTo(cd2.getNomArtiste()) < 0){
+        if (cd1.getNomCD().compareTo(cd2.getNomCD()) < 0){
             res = true;
         }
 
